d9df13172d67074690048361bd180a0a91d24f54
Grasswyq <48287257+Grasswyq@users.noreply.github.com>
2019-04-17 20:09:34 +0800

    Add files via upload

---
+++ ConsoleApplication5.cpp
@@ -0,0 +1,80 @@
+﻿
+//programming by pingline & undef
+#include "pch.h"
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <graphics.h>
+
+using namespace std;
+
+particle pa[100];
+double hp = 0;
+
+void start(int m)
+{
+	srand(time(NULL));
+	initgraph(1200, 1200, SHOWCONSOLE);
+	setorigin(600, 0);
+	if (m == 1)
+	{
+		double rStep_x = plane_X / 11, rStep_y = plane_Y / 11;
+		for (int i = 1; i < 11; ++i)
+		{
+			for (int j = 1; j < 11; ++j)
+			{
+				pa[i * 10 + j - 11].x = i * rStep_x;
+				pa[i * 10 + j - 11].y = j * rStep_y;
+				pa[i * 10 + j - 11].vx = random()*Temp;
+				pa[i * 10 + j - 11].vy = random()*Temp;
+			}
+		}
+	}
+}
+
+void collision(int n)
+{
+	for (int i = 0; i < 100; ++i)
+	{
+		if (i != n)
+		{
+
+		}
+	}
+}
+
+void show()
+{
+	//draw plane
+	line(obd*obx/(oby), obd*(obz-hp)/(oby), obd*(obx - plane_X ) / oby, obd*(obz-hp) /(oby ));
+	line(obd*obx /(oby ), obd*(obz-hp) /(oby ), obd*obx/(oby-plane_Y ), obd*(obz-hp)/(oby-plane_Y ));
+	line(obd*(obx-plane_X)/(oby-plane_Y ),obd*(obz-hp)/(oby-plane_Y ), obd*(obx - plane_X) /(oby ), obd*(obz-hp) /(oby ));
+	line(obd*(obx - plane_X) / (oby - plane_Y ), obd*(obz-hp) / (oby - plane_Y ), obd*obx/(oby-plane_Y ), obd*(obz-hp) /(oby-plane_Y ));
+	//draw particle
+	for (int i = 0; i < 100; ++i)
+	{
+		int px = obd * ((obx-pa[i].x)/(oby-pa[i].y));
+		int py = obd * ((obz - pa[i].z) / (oby-pa[i].y));
+		cout << px << "\t"<< py << "\t"<<pa[i].x<<"\t"<<pa[i].y<<endl;
+		circle(px, py, 3);
+	}
+
+
+}
+
+int main()
+{
+	start(mod);
+	double a = osci_R * T_Step / osci_T;
+	while (true)
+	{
+	show();
+	hp += a;
+	if (abs(hp) > osci_T)
+		a = -a;
+	clearcliprgn();
+	}
+	
+
+}
+
---
+++ pch.h
@@ -0,0 +1,46 @@
+﻿// 入门提示: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+
+#ifndef PCH_H
+#define PCH_H
+
+// TODO: 添加要在此处预编译的标头
+
+
+
+#define osci_T 0.02
+#define osci_R 0.02
+#define plane_X 0.2
+#define plane_Y 0.2
+#define plane_H 0.1
+#define Num 100
+#define mass 0.001
+#define obd 600
+#define obx 0.1
+#define oby 0.3
+#define obz 0.1
+#define mod 1
+/* mod     initial distribution
+	1           uniform
+	2			random
+*/
+#define Temp 0.03
+#define T_Step 0.001
+
+#define random() ((double)rand()/RAND_MAX)
+
+typedef struct particle {
+	double x;
+	double y;
+	double z=0;
+	double vx;
+	double vy;
+	double vz=0;
+};
+
+#endif //PCH_H
